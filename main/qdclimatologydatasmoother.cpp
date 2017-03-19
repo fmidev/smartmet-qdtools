7a502a07c98d282045f74b31d7e0dd6e79d69f11
Marko Pietarinen <marko.pietarinen@fmi.fi>
2017-03-19 11:29:27 +0200

    added new filter that smoothes out one year climatological data. Smoothing is calculated over given days and values over that time period are averaged. Also only the values from the same utc hour is taken account in calculations. This is supposed to use with Ecmwf ERA data to smooth it with 7 days margin.

---
+++ main/qdclimatologydatasmoother.cpp
@@ -0,0 +1,215 @@
+#include <NFmiQueryData.h>
+#include <NFmiCmdLine.h>
+#include <NFmiFastQueryInfo.h>
+#include <NFmiStreamQueryData.h>
+#include <NFmiFileSystem.h>
+#include <NFmiQueryDataUtil.h>
+#include <NFmiTotalWind.h>
+#include <NFmiTimeList.h>
+#include <NFmiDataModifierAvg.h>
+
+#include <fstream>
+
+using namespace std;
+
+// ----------------------------------------------------------------------
+// Kaytto-ohjeet
+// ----------------------------------------------------------------------
+void Usage(void)
+{
+    cout << "Usage: qdclimatologydatasmoother [options] qdin qdout" << endl;
+    cout << "qdin must have annual climatological data. It can be station or grid data." << endl;
+    cout << "Smoothing is calculated by calculating average over given days to smooth." << endl;
+    cout << "If day value is 5, average is calculated from 2 days preor and 2 days after." << endl;
+    cout << "the calculation time. Values are taken from those days only from the same utc hour." << endl;
+    cout << "-d days-smoothed <default 3>\tOver how many days is smoothing calculated." << endl;
+    cout << "\tdays-smoothed must be positive odd integer." << endl;
+}
+
+static void MakeErrorAndUsageMessages(const string &errorMessage)
+{
+    cout << errorMessage << endl;
+    ::Usage();
+    // tässä piti ensin tulostaa cout:iin tavaraa ja sitten vasta Usage,
+    // joten en voinut laittaa virheviesti poikkeuksen mukana.
+    throw runtime_error("");
+}
+
+static void MakeCheckDataError(const string &filePath, const string &baseErrorString)
+{
+    string errorString = baseErrorString;
+    errorString += "\nFile was: ";
+    errorString += filePath;
+    ::MakeErrorAndUsageMessages(errorString);
+}
+
+// No checks yet
+static void CheckData(NFmiFastQueryInfo &info, const string &filePath)
+{
+    auto &timeDescriptor = info.TimeDescriptor();
+    auto diffInDays = timeDescriptor.LastTime().DifferenceInDays(timeDescriptor.FirstTime());
+    if(diffInDays < 364 || diffInDays > 365)
+        MakeCheckDataError(filePath, "Error: input data doesn't contains time spread suitable for one year climatplogical data");
+}
+
+
+static unique_ptr<NFmiQueryData> CreateNewEmtyData(NFmiFastQueryInfo &info)
+{
+    return unique_ptr<NFmiQueryData>(NFmiQueryDataUtil::CreateEmptyData(info));
+}
+
+float CalcSmoothedValue(NFmiFastQueryInfo &sourceInfo, const std::vector<unsigned long> &timeIndices)
+{
+    NFmiDataModifierAvg avg;
+    for(auto timeIndex : timeIndices)
+    {
+        sourceInfo.TimeIndex(timeIndex);
+        avg.Calculate(sourceInfo.FloatValue());
+    }
+    return avg.CalculationResult();
+}
+
+static std::vector<unsigned long> MakeTimeIndexVector(NFmiFastQueryInfo &info, int smoothedDays)
+{
+    auto originalTimeIndex = info.TimeIndex();
+    std::vector<unsigned long> calculatedTimeIndices;
+    auto backwardDaysCount = (smoothedDays - 1) / 2;
+    NFmiMetTime wantedTime = info.Time();
+    wantedTime.ChangeByDays(-backwardDaysCount);
+    for(int i = 0; i < smoothedDays; i++, wantedTime.ChangeByDays(1))
+    {
+        if(info.Time(wantedTime))
+            calculatedTimeIndices.push_back(info.TimeIndex());
+        else
+        {
+            // Lets try both ends of the year to find wanted time
+            // Remember that we have year long climatology data, and we have to look data over the ends from the other side of data.
+            NFmiMetTime previousYearTime(wantedTime);
+            previousYearTime.SetYear(previousYearTime.GetYear() - 1);
+            if(info.Time(previousYearTime))
+                calculatedTimeIndices.push_back(info.TimeIndex());
+            else
+            {
+                NFmiMetTime nextYearTime(wantedTime);
+                nextYearTime.SetYear(nextYearTime.GetYear() + 1);
+                if(info.Time(nextYearTime))
+                    calculatedTimeIndices.push_back(info.TimeIndex());
+                else
+                {
+                    std::cout << "\nNon found time with time-index " << originalTimeIndex << " and time " << wantedTime << ", error in data?" << std::endl;
+//                    ::MakeErrorAndUsageMessages("Error: It seems that given data doesn't have the right year long data set, stopping execution...");
+                }
+            }
+        }
+    }
+
+    // Must restore original timeIndex at end
+    info.TimeIndex(originalTimeIndex);
+    return calculatedTimeIndices;
+}
+
+static void FillOneTimeStep(NFmiFastQueryInfo &destinationInfo, NFmiFastQueryInfo &sourceInfo, const std::vector<unsigned long> &timeIndices)
+{
+    for(destinationInfo.ResetParam(), sourceInfo.ResetParam(); destinationInfo.NextParam() && sourceInfo.NextParam(); )
+    {
+        for(destinationInfo.ResetLevel(), sourceInfo.ResetLevel(); destinationInfo.NextLevel() && sourceInfo.NextLevel(); )
+        {
+            for(destinationInfo.ResetLocation(), sourceInfo.ResetLocation(); destinationInfo.NextLocation() && sourceInfo.NextLocation(); )
+            {
+                destinationInfo.FloatValue(::CalcSmoothedValue(sourceInfo, timeIndices));
+            }
+        }
+    }
+}
+
+static void FillNewData(const unique_ptr<NFmiQueryData> &newData, NFmiFastQueryInfo &sourceInfo, int smoothedDays)
+{
+    NFmiFastQueryInfo destInfo(newData.get());
+    sourceInfo.First();
+    // Time must be the out most loop, because we have to calculate for each time its own timeIndex list 
+    // for those times that will be used for smoothing calculations.
+    for(destInfo.ResetTime(); destInfo.NextTime(); )
+    {
+        auto timeIndex = destInfo.TimeIndex();
+        std::cout << timeIndex << " ";
+        ::FillOneTimeStep(destInfo, sourceInfo, ::MakeTimeIndexVector(destInfo, smoothedDays));
+    }
+}
+
+static unique_ptr<NFmiQueryData> CreateFilteredData(NFmiFastQueryInfo &info, int smoothedDays)
+{
+    unique_ptr<NFmiQueryData> newData = ::CreateNewEmtyData(info);
+    ::FillNewData(newData, info, smoothedDays);
+    return newData;
+}
+
+static void WriteQueryData(unique_ptr<NFmiQueryData> &data, const string &filePath)
+{
+    if(!data)
+        ::MakeErrorAndUsageMessages("Error: unable to create filtered data.");
+    else
+    {
+        if(!NFmiFileSystem::CreateDirectory(NFmiFileSystem::PathFromPattern(filePath)))
+        {
+            string errorMessage = "Error: unable to create target directory in:\n";
+            errorMessage += NFmiFileSystem::PathFromPattern(filePath);
+            ::MakeErrorAndUsageMessages(errorMessage);
+        }
+        NFmiStreamQueryData sqData;
+        if(!sqData.WriteData(filePath, data.get(), static_cast<long>(data->InfoVersion())))
+        {
+            string errorMessage = "Error: unable to write data to file:\n";
+            errorMessage += filePath;
+            ::MakeErrorAndUsageMessages(errorMessage);
+        }
+    }
+}
+
+void Domain(int argc, const char *argv[])
+{
+  NFmiCmdLine cmdline(argc, argv, "d!");
+
+  // Tarkistetaan optioiden oikeus:
+  if (cmdline.Status().IsError())
+  {
+      string errorMessage("Error: Invalid command line:\n");
+      errorMessage += cmdline.Status().ErrorLog();
+      ::MakeErrorAndUsageMessages(errorMessage);
+  }
+  if(cmdline.NumberofParameters() != 2)
+  {
+      ::MakeErrorAndUsageMessages("Error: Invalid command line argument count, must have 2 parameters (qdin qdout)");
+  }
+  string fileNameIn = cmdline.Parameter(1);
+  string fileNameOut = cmdline.Parameter(2);
+  int smoothedDays = 3;
+  if(cmdline.isOption('d'))
+  {
+      smoothedDays = boost::lexical_cast<int>(cmdline.OptionValue('d'));
+      if(smoothedDays < 3)
+          ::MakeErrorAndUsageMessages("Error: Invalid smoothed-days value given, must be positive odd number and at least 3");
+      if(smoothedDays % 2 == 0)
+          ::MakeErrorAndUsageMessages("Error: Invalid smoothed-days value given, must be odd number");
+      if(smoothedDays > 50)
+          ::MakeErrorAndUsageMessages("Error: Invalid smoothed-days value given, can't be over 50 days, that would give questianable results");
+  }
+  NFmiQueryData qData(fileNameIn);
+  NFmiFastQueryInfo info(&qData);
+  ::CheckData(info, fileNameIn);
+  unique_ptr<NFmiQueryData> newData = CreateFilteredData(info, smoothedDays);
+  ::WriteQueryData(newData, fileNameOut);
+}
+
+int main(int argc, const char *argv[])
+{
+    try
+    {
+        ::Domain(argc, argv);
+    }
+    catch(exception &e)
+    {
+        cout << e.what() << endl;
+        return 1;
+    }
+    return 0;  // homma meni putkeen palauta 0 onnistumisen merkiksi!!!!
+}

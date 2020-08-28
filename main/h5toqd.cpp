95222e697f73eed3906a3f4a5600c6248d971eb9
Mikko Partio <mikko.partio@fmi.fi>
2020-08-28 09:07:23 +0300

    -1 indicates failure in library call

--- main/h5toqd.cpp
+++ main/h5toqd.cpp
@@ -463,7 +463,7 @@ bool is_group_attribute(const hid_t &hid, const std::string &path, const std::st
 
   H5Lite::closeId(hid_group, H5G_GROUP);
 
-  return ret != 0;
+  return ret == 1;
 }
 
 template <typename T>
